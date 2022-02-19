a0e30148b9bd0d77103bc98fc55dce5cfa437d79
kinokomushroom <kinokojoe@gmail.com>
2022-02-20 05:29:57 +0900

    Finishing up art region

--- Projector.gd
+++ Projector.gd
@@ -2,15 +2,14 @@ extends Node2D
 
 
 onready var video_player: VideoPlayer = $Viewport/VideoPlayer
-export var dim_color: Color
 onready var viewport_texture: ViewportTexture = $Viewport.get_texture()
 
 
 func _ready():
 	video_player.play()
 	$Temp.visible = false
-#	video_player.paused = true
-	video_player.modulate = dim_color
+	video_player.paused = true
+	video_player.modulate = Color.black
 	
 	$Video.texture = viewport_texture
 	$ProjectorLight.material.set_shader_param("Video", viewport_texture)
@@ -19,15 +18,15 @@ func _ready():
 # triggered when player gets within range
 # brightens projector and resume playback
 func _on_EventTriggerArea_body_entered(body):
-#	video_player.paused = false
+	video_player.paused = false
 	video_player.modulate = Color.white
 
 
 # triggered when player exits range
 # dims projector and pause playback
 func _on_EventTriggerArea_body_exited(body):
-#	video_player.paused = true
-	video_player.modulate = dim_color
+	video_player.paused = true
+	video_player.modulate = Color.black
 
 
 # reset video when playback ends
