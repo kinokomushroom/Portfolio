48fb0691dfad459b175a863b7173775f04982782
kinokomushroom <kinokojoe@gmail.com>
2022-02-22 15:36:05 +0900

    Added touch input

--- Player.gd
+++ Player.gd
@@ -6,8 +6,8 @@ onready var animationplayer: AnimationPlayer = $RenderPos/Keys/AnimationPlayer
 var has_moved: bool = false
 
 
-func move_player() -> void:
-	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+func move_player(input_vector: Vector2) -> void:
+#	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var player_velocity: Vector2 = player_speed * input_vector
 	move_and_slide(player_velocity)
 	$RenderPos.global_position = global_position.round() # pixel perfect movement
@@ -25,4 +25,5 @@ func _ready() -> void:
 
 
 func _physics_process(delta) -> void:
-	move_player()
+	var input_vector: Vector2 = Vector2(int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")), int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))).normalized()
+	move_player(input_vector)
